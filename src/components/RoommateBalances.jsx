ff9c8f874e29f0ad34cec49f492de8291cfbf036
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2026-03-04 00:02:12 +0800

    adding RoomateBalances component

---
+++ src/components/RoommateBalances.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const RoommateBalances = () => {
+  return (
+    <ul className='border borde-white px-6 py-4'>
+        <li>
+            <img src="" alt="" />
+            <p>Charles</p>
+            <p>Balanc: +2</p>
+        </li>
+
+    </ul>
+  )
+}
+
+export default RoommateBalances
